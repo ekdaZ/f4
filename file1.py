6908fb05eaa02d023091e866628ed0c94958cc44
Leonid <109795951+ekdaZ@users.noreply.github.com>
2024-04-13 13:57:08 +0100

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,1 +1,11 @@
-heeeyy
+import icalendar
+import urllib.request
+
+
+
+def calendar_import(calendar_link):
+    contents = icalendar.Calendar.from_ical(urllib.request.urlopen(calendar_link).read())
+    print(contents)
+    return 0
+
+calendar_import('https://mytimetable.bath.ac.uk/ical?eu=bHoyMDkxQGJhdGguYWMudWs=&h=KGJcLYfDMYrEkRccsDpsc5XKA1kwIcsb3nEI5Ebh_FM=')
