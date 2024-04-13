7bbb248561553d7394f3ee5c77fb9e52f0a4ecd5
will-yang-zh <william-yang.zh@outlook.com>
2024-04-13 16:28:46 +0100

    get week complete

--- backend/work_cal.py
+++ backend/work_cal.py
@@ -16,12 +16,12 @@ def event_to_dict(event):
         completion = True
     return {
         'name': event.name,
+        'week': event.begin.date().strftime('%U'),
         'day': event.begin.date().strftime('%Y-%m-%d'),
         'location': event.location,
         # 'Description': event.description,
         'begin': event.begin,
         'end': event.end,
-        # 'day': event.begin.date().strftime('%Y-%m-%d
         'priority': 'high',
         'completion': completion,
     }
@@ -37,18 +37,25 @@ def ics_to_csv():
 # calendar_link = 'https://mytimetable.bath.ac.uk/ical?eu=bHoyMDkxQGJhdGguYWMudWs=&h=KGJcLYfDMYrEkRccsDpsc5XKA1kwIcsb3nEI5Ebh_FM='
 
 
+
 def read_calendar():
     df = pd.DataFrame(ics_to_csv())
     df.to_pickle('backend/table')
  
 
-def get_week():
-    datetime.now(timezone.utc)
+def get_week(week):
+    df = pd.read_pickle('backend/table')
+    return df.loc[df['week'] == week]
 
 def get_day(day):
     df = pd.read_pickle('backend/table')
     return df.loc[df['day'] == day]
 
+def post_cw():
+
+
+    return 0
+
 print(read_calendar())
 
-print(get_day('2024-04-15'))
+print(get_week('15'))
