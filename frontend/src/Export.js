facfba2968a509470dd35bd54d8fb396c94a9699
yuyulyu <yuyulyu0730@gmail.com>
2024-04-14 11:01:58 +0100

    Update Input.js

--- frontend/src/Export.js
+++ frontend/src/Export.js
@@ -10,74 +10,24 @@ export default function Export() {
     EventRegister.emit("closeModal", "Export");
   };
 
-  const styles = StyleSheet.create({
-    container: {
-      padding: 10
-    },
-    row: {
-      flexDirection: 'row', // Set direction of inner items to row
-      justifyContent: 'space-between', // Space items uniformly
-      alignItems: 'center', // Align items vertically
-      marginTop: 10 // Add top margin for spacing between rows
-    },
-    textInput: {
-      flex: 1, // Take up all available space
-      borderColor: 'gray',
-      borderWidth: 1,
-      padding: 8,
-    },
-    fullWidth: {
-      flex: 1, // Ensure this element takes the full width
-    },
-    label: {
-      marginRight: 10
-    }
-  });
-
   return (
     <View className="bg-gray-50 trans absolute -mt-[600px]" style={{width:"70%"}}>
       <View className="border rounded p-4 shadow-xl">
         <View className="grid grid-cols-2 grid-rows-4 gap-2">
-        <View style={styles.container}>
-      <TextInput
-        placeholder="Export"
-        placeholderTextColor="gray"
-        width = "auto"
-        fontWeight = "bold"
-        fontSize = "20"
-      />
+        <View>
+      <Text style={{fontWeight:"bold",fontSize:20}}>Export</Text>
       
+      <View style={{width:"90%",backgroundColor:"#d8d8d8",alignSelf:"center",marginVertical:20,padding:10}}>
+        <Text style={{fontSize:15}}>https://example.com/mycalendar.ics</Text>
+      </View>
       
-      <View style={styles.row}>
-        <Text style={styles.label}>Start Date:</Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="yyyy-mm-dd"
-        />
-      </View>
-
-      <View style={styles.row}>
-        <Text style={styles.label}>End Date:</Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="yyyy-mm-dd"
-        />
-      </View>
-
-      <View style={styles.row}>
-        <Text style={styles.label}>Duration:</Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="num of hrs"
-        />
-      </View>
       </View>
         </View>
 
           <View className="border border-gray-300 rounded shadow-2xl  ">
             <Button
               className=""
-              title="submit"
+              title="copy"
               onPress={() => closeModal()}
             ></Button>
           </View>
